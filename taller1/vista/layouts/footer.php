a005eec48d98c965bc112d5ebc39014da2f1966c
JONATHANDEMOND <133927721+JONATHANDEMOND@users.noreply.github.com>
2023-05-17 20:25:24 -0500

    Update footer.php

--- taller1/vista/layouts/footer.php
+++ taller1/vista/layouts/footer.php
@@ -1,6 +1,10 @@
        <!-- /contenido-->
     
        </div> 
+<h1 class="text-center">GRACIAS POR USAR NUESTRO SERVICIO</h1>
+<h1 class="text-center">---------------------------------</h1>
+
+
 
 </body>
 </html>
