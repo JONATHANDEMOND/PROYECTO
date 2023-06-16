329fc5ae181182f86875e4f2cab698303ca2ce84
JONATHANDEMOND <jo_u123@outlook.es>
2023-06-16 13:31:43 -0500

    BSCKGROUND

--- src/screen/DetailProductsScreen.styles.js
+++ src/screen/DetailProductsScreen.styles.js
@@ -2,11 +2,12 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   
     content: {
-    backgroundColor: "#FFA07A",
+    backgroundColor: "#5B779B",
     marginHorizontal: 20,
     marginTop: 30,
   },
   input: {
+    backgroundColor: "#6F7999",
     width: "100%",
     textAlign: "center",
     marginTop: 20,
